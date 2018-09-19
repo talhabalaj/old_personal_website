028005c21d6c36d727f71889508ac40b42c06ca8
Talha Balaj <talhabalaj@hotmail.com>
2018-09-19 12:42:57 +0500

    added css and js

---
+++ public/js/script.js
@@ -0,0 +1,45 @@
+function sleep (time) {
+  return new Promise((resolve) => setTimeout(resolve, time));
+}
+
+async function backspaceText(container) {
+  container.classList.toggle('typing');
+  const textArray = container.textContent.split('');
+  const letterDuration = 2500 / textArray.length;
+  
+  for (let textLetter in textArray) {
+    const index = (textArray.length - textLetter);
+    container.innerHTML = container.innerHTML.slice(0, index - 1);
+    await sleep(letterDuration);
+  }
+  
+  container.classList.toggle('typing');
+}
+
+async function typeItThere(container, text, duration) {  
+  const textArray = text.split('');
+  const letterDuration = duration / textArray.length; 
+  
+  container.classList.toggle('typing');
+  
+  for (let textLetter of textArray) {
+    container.innerHTML += textLetter;
+    await sleep(letterDuration);
+  }
+  
+  container.classList.toggle('typing');
+}
+
+(async ()=>{
+  const myInfoContainer = document.querySelector('#my-info')
+  const myInfoArray = ["Full-Stack Web Developer", "Computer Sci. Student at PUCIT"]
+  while (true) {
+    for (const myInfoString of myInfoArray) {
+      await typeItThere(myInfoContainer, myInfoString, 2500);
+      await sleep(500);
+      await backspaceText(myInfoContainer);   
+    }
+  } 
+})();
+
+
